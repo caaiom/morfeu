2daa0abc80e679cabe34f337aa9f95d562ef2022
Caio Maia <caaiomaia@gmail.com>
2016-04-14 01:27:33 -0300

    API First Blood

---
+++ web/src/main/java/br/com/morfeu/api/Application.java
@@ -0,0 +1,18 @@
+package br.com.morfeu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by cmaia on 14/04/16
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("br.com.morfeu.api.controller")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
