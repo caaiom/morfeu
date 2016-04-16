5fefae12bd880d98203f0f925acafafabbadc507
Caio Maia <cmaia@uolinc.com>
2016-04-15 21:57:34 -0300

    Database in view and adding angular to submit via ajax

---
+++ web/src/main/resources/static/js/generate.js
@@ -0,0 +1,8 @@
+var app = angular.module('morfeu', []);
+
+app.controller('GenerateCtrl', function($scope) {
+    $scope.submit = function() {
+        //do something
+        console.log("ok");
+    };
+});
