2384d0a3af0227fec7c06bf861b0a2e37eb4db3a
Caio Maia <caaiomaia@gmail.com>
2016-04-16 17:39:29 -0300

    Angular databind and submission form to backend

--- web/src/main/resources/static/js/generate.js
+++ web/src/main/resources/static/js/generate.js
@@ -1,8 +1,43 @@
 var app = angular.module('morfeu', []);
+var url = "/generate";
 
-app.controller('GenerateCtrl', function($scope) {
+app.controller('GenerateCtrl', function($scope, $http) {
+    $scope.data = {
+        selectedLanguage: null,
+        availableLanguages: [
+            {id: "java", name: "Java"},
+            {id: "python", name: "Python"},
+            {id: "ruby", name: "Ruby"},
+            {id: "javascript", name: "Javascript (Node.js)"}
+        ],
+        selectedDatabase: null,
+        availableDatabases: [
+            {id: "mysql", name: "MySQL"},
+            {id: "postgres", name: "PostgreSQL"},
+            {id: "oracle", name: "Oracle"}
+        ],
+        connection: ""
+    };
+
+    $scope.servicePayload = {
+        language : "",
+        database: "",
+        connection: ""
+    };
+
     $scope.submit = function() {
-        //do something
-        console.log("ok");
+        $scope.servicePayload.language = $scope.data.selectedLanguage;
+        $scope.servicePayload.database = $scope.data.selectedDatabase;
+        $scope.servicePayload.connection = $scope.data.connection;
+
+        console.log($scope.servicePayload);
+
+        $http.post(url, JSON.stringify($scope.servicePayload), {
+            headers: {"Content-type":"application/json"}
+        }).then(function() {
+                console.log("success");
+            }, function() {
+                console.log("error");
+            });
     };
 });
